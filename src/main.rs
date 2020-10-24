e21e55ff1e547cf951de2e93c9391e4bf8224fe0
Tuser Sheikh <tuser.sheikh@zoho.com>
2020-10-24 15:35:18 +0600

    panagram check

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 mod string;
 
 fn main() {
-    let reverse_word = string::reverse_word("Harry Potter");
-    println!("{:?}", reverse_word);
+    // let reverse_word = string::reverse_word("Harry Potter");
+    // println!("{:?}", reverse_word);
+
+    let is_panagram = string::panagram_check("The quick brown fox jumps over the lazy dog");
+    println!("{:?}", is_panagram);
+    let is_panagram = string::panagram_check("I’ve always wanted to use that spell!");
+    println!("{:?}", is_panagram);
 }
--- src/string.rs
+++ src/string.rs
@@ -1,3 +1,22 @@
 pub fn reverse_word(string: &str) -> String {
 	string.chars().rev().collect()
 }
+
+pub fn panagram_check(string: &str) -> bool {
+	let mut letters: [bool; 26] = [false; 26];
+
+	for c in string.to_lowercase().chars() {
+		if c.is_ascii_alphabetic() {
+			let index = (c as usize) - 97;
+			letters[index] = true;
+		}
+	}
+
+	for c in &letters {
+		if !c {
+			return false;
+		}
+	}
+
+	true
+}
